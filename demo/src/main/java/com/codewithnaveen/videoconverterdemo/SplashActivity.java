47bbbbe84a90c1842753316dcd41662be8ce49ab
Naveen364 <naveen364@users.noreply.github.com>
2020-07-29 21:12:18 +0530

    updated

--- demo/src/main/java/com/codewithnaveen/videoconverterdemo/SplashActivity.java
+++ demo/src/main/java/com/codewithnaveen/videoconverterdemo/SplashActivity.java
@@ -1,19 +1,36 @@
 package com.codewithnaveen.videoconverterdemo;
 
 import android.app.Activity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
 
 
 
 public class SplashActivity extends Activity  {
-
+    private static boolean splashLoaded = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (!splashLoaded) {
+            setContentView(R.layout.activity_splash);
+            int secondsDelayed = 1;
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }, secondsDelayed * 500);
 
+            splashLoaded = true;
+        }
+        else {
+            Intent goToMainActivity = new Intent(SplashActivity.this, MainActivity.class);
+            goToMainActivity.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(goToMainActivity);
+            finish();
+        }
     }
 }
