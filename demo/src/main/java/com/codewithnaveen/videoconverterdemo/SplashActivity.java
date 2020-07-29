e4df33d2a718f160fe5ee27aa108d0118fa2dd15
Naveen364 <naveen364@users.noreply.github.com>
2020-07-29 20:57:37 +0530

    Added Splash Screen

---
+++ demo/src/main/java/com/codewithnaveen/videoconverterdemo/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.codewithnaveen.videoconverterdemo;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+
+
+
+public class SplashActivity extends Activity  {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
