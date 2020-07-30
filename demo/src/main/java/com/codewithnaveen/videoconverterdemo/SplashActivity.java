de4742bfa1d22ddf9b2ba6c37e35bcea6a132e31
Naveen364 <naveen364@users.noreply.github.com>
2020-07-30 19:08:20 +0530

    Added animation

--- demo/src/main/java/com/codewithnaveen/videoconverterdemo/SplashActivity.java
+++ demo/src/main/java/com/codewithnaveen/videoconverterdemo/SplashActivity.java
@@ -4,25 +4,28 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 
 public class SplashActivity extends Activity  {
     private static boolean splashLoaded = false;
-
+    Animation logo_move;
+    ImageView imageView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         if (!splashLoaded) {
             setContentView(R.layout.activity_splash);
-            int secondsDelayed = 1;
+            int secondsDelayed = 2;
             new Handler().postDelayed(new Runnable() {
                 public void run() {
                     startActivity(new Intent(SplashActivity.this, MainActivity.class));
                     finish();
                 }
-            }, secondsDelayed * 500);
+            }, secondsDelayed * 1000);
 
             splashLoaded = true;
         }
@@ -32,5 +35,10 @@ public class SplashActivity extends Activity  {
             startActivity(goToMainActivity);
             finish();
         }
+
+        imageView = (ImageView) findViewById(R.id.imageView1);
+        logo_move = AnimationUtils.loadAnimation(this,R.anim.logo_move);
+
+        imageView.setAnimation(logo_move);
     }
 }
